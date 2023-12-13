bdb3c55dc1b9e2e99c6803ac833b5b33ce76707a
Juliana <jflorez@proton.me>
2023-12-13 09:33:18 -0500

    Creación del formulario de creación de usuario

--- client/src/Components/UserForm/UserForm.jsx
+++ client/src/Components/UserForm/UserForm.jsx
@@ -1,11 +1,57 @@
 function UserForm() {
     return (
-      <>
-        <div>
-            <h1>Form</h1>
-        </div>
-      </>
+        <>
+            <div>
+                <h1>Form</h1>
+                <div>
+                    <form>
+                        <label>Nombre:  </label>
+                        <input
+                            type="text"
+                            name="name"
+                            placeholder="Ingrese su nombre"
+                        />
+                        <label>Apellido:  </label>
+                        <input
+                            type="text"
+                            name="lastname"
+                            placeholder="Ingrese su apellido"
+                        />
+                        <label>Correo Electrónico:  </label>
+                        <input
+                            type="text"
+                            name="email"
+                            placeholder="Ingrese su correo electrónico"
+                        />
+                        <label>Tipo de documento:</label>
+                        <select
+                            name="documento"
+                        >
+                            <option value="">Selecciona un documento</option>
+                            <option value="dni">DNI</option>
+                            <option value="ce">CE</option>
+                            <option value="pasaporte">Pasaporte</option>
+                        </select>
+
+                        <label>Número de documento:  </label>
+                        <input
+                            type="text"
+                            name="DNI"
+                            placeholder="Ingrese su numero de documento"
+                        />
+                         <label>Teléfono:  </label>
+                        <input
+                            type="text"
+                            name="DNI"
+                            placeholder="Ingrese su teléfono de contacto"
+                        />
+                    </form>
+                </div>
+                <h1> o ingresa con</h1>
+                <h1> sección para poner terceros</h1>
+            </div>
+        </>
     );
-  }
-  
-  export default UserForm;
+}
+
+export default UserForm;
