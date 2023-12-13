7369f07227400cbeceb9f92e1bba51087700ae68
Juan.ruize <juan.ruize@gmail.com>
2023-12-12 20:45:40 -0500

    rutas back

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,5 +1,11 @@
 const { Router } = require('express')
+const productRouter = require ('./productRouter')
+const userRouter = require ('./userRuter')
+
 
 const router = Router()
+
+router.use("/product", productRouter)
+router.use("/user", userRouter)
 
 module.exports = router
