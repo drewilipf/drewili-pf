47fb0515f227f2afef284292446ee54f64955def
Sergio Vallenari <vallenarisergioio@gmail.com>
2023-12-14 20:16:49 -0300

    update modelos y relaciones

--- server/index.js
+++ server/index.js
@@ -2,8 +2,8 @@ const server = require('./src/server.js')
 const PORT = 3001
 const {conn} = require('./src/db.js')
 
-conn.sync({ force: true }).then(() => {
+conn.sync({ force: false }).then(() => {
     server.listen(PORT, () => {
-      console.log('%s listening at 3001'); // eslint-disable-line no-console
+      console.log('%s listening at' + PORT); // eslint-disable-line no-console
     });
   });
