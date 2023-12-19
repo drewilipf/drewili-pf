0238a3f398501ca78aebcea77f867f6e59b28831
Sergio Vallenari <vallenarisergioio@gmail.com>
2023-12-19 20:41:24 -0300

    Controlador de filtros por categoria creados

---
+++ server/src/controllers/Product/filterCategoryController.js
@@ -0,0 +1,18 @@
+const { Category, Product } = require("../../db")
+
+const filterProductCategory = async(category) =>{
+    const selectedCategory = await Category.findOne({
+        where:{category: category}
+    })
+    const products = await Product.findAll({
+        where: {
+          category_id: selectedCategory.id,
+          deleted: false,
+        },
+      });
+  
+      return products;
+  
+}
+
+module.exports = filterProductCategory
