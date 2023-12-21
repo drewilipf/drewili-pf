db5d2eee207040c7ea21340982d0281dc87bd417
Juliana <jflorez@proton.me>
2023-12-21 18:53:04 -0500

    el detail ya tiene casi todos los estilos, pero estos tienen bugs

--- client/src/Components/DetailComponents/CommentInput.jsx
+++ client/src/Components/DetailComponents/CommentInput.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { useDispatch } from "react-redux";
 import { postComment } from "../../reduxToolkit/Comment/commentThunks";
 
+
 const CommentInput = ({ user_id, product_id }) => {
   const dispatch = useDispatch();
   const [comment, setComment] = useState("");
@@ -21,18 +22,24 @@ const CommentInput = ({ user_id, product_id }) => {
   };
 
   return (
-    <div>
+    <div className="">
       <textarea
-        rows="4"
-        cols="50"
+        className=" border-chiliRed border p-4 mt-4 rounded-lg w-4/5 h-1/2 rounded-md p-2 text-black font-arial text-base"
+        rows="3"
         placeholder="Deja tu comentario..."
         value={comment}
         onChange={handleCommentChange}
       ></textarea>
       <br />
-      <button onClick={handlePostComment}>Enviar Comentario</button>
+      <button
+        className="bg-chiliRed text-whiteSmoke rounded-md w-1/5 p-2 mt-2 hover:shadow-lg"
+        onClick={handlePostComment}
+      >
+        Enviar
+      </button>
     </div>
   );
 };
 
 export default CommentInput;
+
