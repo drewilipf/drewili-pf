d718384b2918431f07113c4948dd34b8fceb71fa
Juan.ruize <juan.ruize@gmail.com>
2023-12-22 21:03:56 -0500

    filter brand

--- server/src/routes/filterRouter.js
+++ server/src/routes/filterRouter.js
@@ -1,8 +1,10 @@
 const {Router} = require('express')
 const filterCategoryHandler = require('../handlers/Product/filterCategoryHandler')
+const getFilterBrandHandler= require('../handlers/Product/getFilterBrandHandler')
 const router = Router()
 
 router.get('/category', filterCategoryHandler)
+router.get("/brand", getFilterBrandHandler)
 
 
 module.exports = router
