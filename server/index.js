d219591bfabee69d23438a922d172e5edb6a4545
gabrielaDF <defreitasgabi@gmail.com>
2023-12-29 02:18:19 -0500

    antes del pull

--- server/index.js
+++ server/index.js
@@ -1,9 +1,9 @@
-const server = require('./src/server.js')
-const PORT = 3001
-const {conn} = require('./src/db.js')
+const server = require("./src/server.js");
+const PORT = 3001;
+const { conn } = require("./src/db.js");
 
-conn.sync({ force: false }).then(() => {
-    server.listen(PORT, () => {
-      console.log('%s listening at ' + PORT); // eslint-disable-line no-console
-    });
+conn.sync({ alter: true }).then(() => {
+  server.listen(PORT, () => {
+    console.log("%s listening at " + PORT); // eslint-disable-line no-console
   });
+});
--- server/src/controllers/Product/postProductController.js
+++ server/src/controllers/Product/postProductController.js
@@ -1,31 +1,45 @@
-require('dotenv').config()
-const { Product } = require('../../db');
-const cloudinary = require('cloudinary')
-const {CLOUD_NAME, API_KEY, API_SECRET} = process.env
+require("dotenv").config();
+const { Product } = require("../../db");
+const cloudinary = require("cloudinary");
+const { CLOUD_NAME, API_KEY, API_SECRET } = process.env;
+
 cloudinary.config({
-    CLOUD_NAME, API_KEY, API_SECRET
-})
+  CLOUD_NAME,
+  API_KEY,
+  API_SECRET,
+});
 
-const postProductsController = async (name, description, price, specifications, stock, image, brand_id, category_id, color_id, relevance) => {
+const postProductsController = async (
+  name,
+  description,
+  price,
+  specifications,
+  stock,
+  image,
+  brand_id,
+  category_id,
+  color_id,
+  relevance
+) => {
+  const cloudinaryUpload = await cloudinary.uploader.upload(image);
 
-    const cloudinaryUpload = await cloudinary.uploader.upload(image)
+  const imageUrl = cloudinaryUpload.secure_url;
 
-    const imageUrl = cloudinaryUpload.secure_url
+  const newProduct = await Product.create({
+    name,
+    description,
+    price,
+    specifications,
+    stock,
+    image: imageUrl,
+    color_id,
+    brand_id,
+    category_id,
+    relevance,
+  });
+  console.log(newProduct);
 
-    const newProduct = await Product.create({
-        name,
-        description,
-        price,
-        specifications,
-        stock,
-        image: imageUrl,
-        color_id,
-        brand_id,
-        category_id,
-        relevance,
-    });
-
-    return newProduct;
+  return newProduct;
 };
 
 module.exports = postProductsController;
