e899f8beb3b022a57e2d306a099833d622349803
Sergio Vallenari <vallenarisergioio@gmail.com>
2023-12-29 23:25:54 -0300

    Correccion de controladores

--- server/src/controllers/filterController.js
+++ server/src/controllers/filterController.js
@@ -3,69 +3,71 @@ const { Category, Brand, Colors, Product } = require("../db");
 
 
 const filterController = async (filters) => {
-    try {
-        const { category, brand, color, minPrice, maxPrice } = filters;
+  try {
+    const { category, brand, color, minPrice, maxPrice } = filters;
 
 
-        const filterConditions = {};
-    
-        if (category) {
-          filterConditions["$Category.category$"] = category;
-        }
-    
-        if (brand) {
-          filterConditions["$Brand.brand$"] = brand;
-        }
-    
-        if (color) {
-          filterConditions["$Colors.color$"] = color;
-        }
-    
-        if (minPrice && maxPrice) {
-            filterConditions.price = {
-              [Op.between]: [parseFloat(minPrice), parseFloat(maxPrice)],
-            };
-          }
-    
-        const products = await Product.findAll({
-          include: [
-            {
-              model: Category,
-              attributes: ["category"],
-              as: "Category", // Agrega un alias para la tabla Category
-            },
-            {
-              model: Brand,
-              attributes: ["brand"],
-              as: "Brand", // Agrega un alias para la tabla Brand
-            },
-            {
-              model: Colors,
-              attributes: ["color"],
-              as: "Colors", // Agrega un alias para la tabla Colors
-            },
-          ],
-          where: filterConditions
-        });
+    const filterConditions = {};
 
-//   const formattedProducts = products.map((product) => ({
-//     id: product.id,
-//     name: product.name,
-//     description: product.description,
-//     price: product.price,
-//     specifications: product.specifications,
-//     color: product.color.color,
-//     stock: product.stock,
-//     image: product.image,
-//     brand: product.brand.brand,
-//     category: product.category.category,
-//     deleted: product.deleted,
-//     relevance: product.relevance,
-//     date: product.createdAt,
-//   }));
+    if (category) {
+      filterConditions["$Category.category$"] = category;
+    }
 
-  return products;
-} catch (error) {
+    if (brand) {
+      filterConditions["$Brand.brand$"] = brand;
+    }
+
+    if (color) {
+      filterConditions["$Colors.color$"] = color;
+    }
+
+    if (minPrice && maxPrice) {
+      filterConditions.price = {
+        [Op.between]: [parseFloat(minPrice), parseFloat(maxPrice)],
+      };
+    }
+
+    const products = await Product.findAll({
+      include: [
+        {
+          model: Category,
+          attributes: ["category"],
+          as: "Category", // Agrega un alias para la tabla Category
+        },
+        {
+          model: Brand,
+          attributes: ["brand"],
+          as: "Brand", // Agrega un alias para la tabla Brand
+        },
+        {
+          model: Colors,
+          attributes: ["color"],
+          as: "Colors", // Agrega un alias para la tabla Colors
+        },
+      ],
+      where: filterConditions
+    });
+
+    const formattedProducts = products.map((product) => {
+      return {
+        id: product.id,
+        name: product.name,
+        description: product.description,
+        price: product.price,
+        specifications: product.specifications,
+        color: product.Colors.color,  // Accede a la propiedad 'color' dentro de 'Colors'
+        stock: product.stock,
+        image: product.image,
+        brand: product.Brand.brand,  // Accede a la propiedad 'brand' dentro de 'Brand'
+        category: product.Category.category,  // Accede a la propiedad 'category' dentro de 'Category'
+        deleted: product.deleted,
+        relevance: product.relevance,
+        date: product.createdAt,
+      };
+    });
+
+    return formattedProducts;
+  } catch (error) {
     console.error(error);
     throw new Error('Error obteniendo productos');
   }
