473edceb0de5eb60e07e12f175009927e2d5dd65
MauricioJuarezCollado <mauriciosantana962@yahoo.com>
2024-01-03 14:41:31 -0300

    Agregada funcionalidad favs y carrito en home

--- client/src/views/Shoppingcart/Shoppingcart.jsx
+++ client/src/views/Shoppingcart/Shoppingcart.jsx
@@ -99,35 +99,6 @@ const ShoppingCart = () => {
   return (
     <div className="bg-gray-800 text-black p-4 h-90vh">
       <h2 className="text-2xl font-semibold mb-4">Tu Carrito de Compras</h2>
-<<<<<<< HEAD
-      <div className="flex items-center justify-between py-2">
-        <span className="flex-1">Nombre del Producto</span>
-        <span className="w-16 text-right mr-4">Precio</span>
-        <span className="w-16 text-left ml-2">Cantidad</span>
-      </div>
-      {cartItems.map((item) => (
-        <div key={item.salesCartId} className="flex items-center justify-between py-2 space-y-2">
-          <NavLink to={`/detail/${item.productId}`} className="flex items-center flex-1">
-            <img src={item.image} alt={item.name} className="mr-2" style={{ maxWidth: '50px', maxHeight: '50px' }} />
-            {item.name}
-          </NavLink>
-          <span className="w-16 text-right">{`$${parseFloat(item.price).toFixed(2)}`}</span>
-          <span className="w-16 text-right">{item.quantity}</span>
-          <button onClick={() => handleRemoveFromCart(item.salesCartId, item.totalPrice)} className="ml-2">
-            <img src={binIcon} alt="quitar" style={{ maxWidth: '20px', maxHeight: '20px' }} />
-          </button>
-        </div>
-      ))}
-      <div className="mt-4">
-        <div className="flex justify-between">
-          <span className="font-semibold">Total:</span>
-          <span className="text-2xl">{`$${totalCartPrice.toFixed(2)}`}</span>
-        </div>
-        <button className="mt-4 bg-orange-600 hover:bg-orange-700 text-white py-2 px-4 rounded">
-          Pagar
-        </button>
-      </div>
-=======
       {
         cartItems && cartItems.length !== 0 ?
           <>
@@ -165,7 +136,6 @@ const ShoppingCart = () => {
 
       }
 
->>>>>>> 29e3b6b597aebc12b5ab2b316f43c02fd5e60d50
     </div>
   );
 };
