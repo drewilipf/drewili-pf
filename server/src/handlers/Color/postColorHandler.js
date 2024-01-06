974ca30a2f49306221ebf7cd8fd5c1ee0d9a4483
Sergio Vallenari <vallenarisergioio@gmail.com>
2024-01-05 21:43:58 -0300

    Actualizacion de controladores varios

--- server/src/handlers/Color/postColorHandler.js
+++ server/src/handlers/Color/postColorHandler.js
@@ -5,8 +5,8 @@ const postColorHandler = async (req, res) => {
     const {color} = req.body;
 
     try {
-        await postColorController(color);
-        return res.status(200).send("Color creado con éxito")
+        const colors =  await postColorController(color);
+        return res.status(200).json(colors)
     } catch (error) {
         return res.status(500).json({error: error.message})
     }
