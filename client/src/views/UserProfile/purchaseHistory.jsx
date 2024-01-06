29f00f7afc868bdcef82942f5ea79880fbf62746
florencia Arevalo <florarevcor@gmail.com>
2024-01-06 17:37:28 -0300

    historial de compras

---
+++ client/src/views/UserProfile/purchaseHistory.jsx
@@ -0,0 +1,43 @@
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { getPurchaseHistory } from '../../reduxToolkit/purchaseHistory/purchaseHistoryThunks';
+
+const PurchaseHistoryComponent = ({ userId }) => {
+  const dispatch = useDispatch();
+  const purchaseHistory = useSelector((state) => state.purchaseHistory.data);  // This is where the error is occurring
+  const loading = useSelector((state) => state.purchaseHistory.loading);
+  const error = useSelector((state) => state.purchaseHistory.error);
+
+  useEffect(() => {
+    dispatch(getPurchaseHistory(userId));
+  }, [dispatch, userId]);
+
+  if (loading) {
+    return <p>Cargando historial de compras...</p>;
+  }
+
+  if (error) {
+    return <p>Error al cargar el historial de compras: {error}</p>;
+  }
+
+  return (
+    <div>
+      <h2>Historial de Compras</h2>
+      {purchaseHistory.length === 0 ? (
+        <p>No hay compras registradas para este usuario.</p>
+      ) : (
+        <ul>
+          {purchaseHistory.map((purchase) => (
+            <li key={purchase.purchaseId}>
+              <p>Producto: {purchase.productName}</p>
+              <p>Precio: ${purchase.productPrice}</p>
+              <p>Cantidad: {purchase.quantity}</p>
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+};
+
+export default PurchaseHistoryComponent;
