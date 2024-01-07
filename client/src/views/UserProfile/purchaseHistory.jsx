31f1ff5c94fa572e6a757abab9e290baefc74fcf
florencia Arevalo <florarevcor@gmail.com>
2024-01-07 11:11:46 -0300

    tailwind purchaseHistory

--- client/src/views/UserProfile/purchaseHistory.jsx
+++ client/src/views/UserProfile/purchaseHistory.jsx
@@ -2,43 +2,62 @@ import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { getPurchaseHistory } from "../../reduxToolkit/PurchaseHistory/purchaseHistoryThunks";
 import { useParams } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
+
 
 const PurchaseHistoryComponent = () => {
   const dispatch = useDispatch();
-  const { userId } = useParams()
-  console.log(userId, 'id del historial');
-  const purchaseHistory = useSelector((state) => state.purchaseHistory.data);  // This is where the error is occurring
+  const { userId } = useParams();
+  const purchaseHistory = useSelector((state) => state.purchaseHistory.data);
   const loading = useSelector((state) => state.purchaseHistory.loading);
   const error = useSelector((state) => state.purchaseHistory.error);
+
   useEffect(() => {
     dispatch(getPurchaseHistory(userId));
   }, [dispatch, userId]);
 
   if (loading) {
-    return <p>Cargando historial de compras...</p>;
+    return <p className="text-black">Cargando historial de compras...</p>;
   }
 
   if (error) {
-    return <p>Error al cargar el historial de compras: {error}</p>;
+    return <p className="text-red-500">Error al cargar el historial de compras: {error}</p>;
   }
 
   return (
-    <div>
-      <h2>Historial de Compras</h2>
-      {purchaseHistory.length === 0 ? (
-        <p>No hay compras registradas para este usuario.</p>
-      ) : (
-        <ul>
-          {purchaseHistory.map((purchase) => (
-            <li key={purchase.purchaseId}>
-              <p>Producto: {purchase.productName}</p>
-              <p>Precio: ${purchase.productPrice}</p>
-              <p>Cantidad: {purchase.quantity}</p>
-            </li>
-          ))}
-        </ul>
-      )}
-    </div>
+    
+
+<div className="text-black p-8 rounded-lg shadow-lg h-90vh">
+  <h2 className="text-3xl font-semibold mb-6">Historial de Compras</h2>
+  {purchaseHistory.length === 0 ? (
+    <p>No purchase history for this user.</p>
+  ) : (
+    <ul className="space-y-4">
+      {purchaseHistory.map((purchase) => (
+        <li
+          key={purchase.purchaseId}
+          className="flex items-center justify-between py-3 border-b border-chiliRed"
+        >
+          <div className="flex items-center space-x-4">
+            <NavLink to={`/detail/${purchase.productId}`} className="flex items-center space-x-4">
+              <img
+                src={purchase.productImage}
+                alt={purchase.productName}
+                className="w-36 h-36 rounded-md"
+              />
+              <div>
+                <p className="text-lg font-semibold">{purchase.productName}</p>
+                <p>Precio: ${purchase.productPrice}</p>
+                <p>Cantidad: {purchase.quantity}</p>
+              </div>
+            </NavLink>
+
+          </div>
+        </li>
+      ))}
+    </ul>
+  )}
+</div>
   );
 };
 
