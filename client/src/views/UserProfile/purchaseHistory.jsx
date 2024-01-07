4c0d6b0b7716563b6427d120ace5389c9ff35dc0
Sergio Vallenari <vallenarisergio@gmail.com>
2024-01-07 17:15:49 -0300

    modificacion css

--- client/src/views/UserProfile/purchaseHistory.jsx
+++ client/src/views/UserProfile/purchaseHistory.jsx
@@ -25,39 +25,45 @@ const PurchaseHistoryComponent = () => {
   }
 
   return (
-    
 
-<div className="text-black p-8 rounded-lg shadow-lg h-90vh">
-  <h2 className="text-3xl font-semibold mb-6">Historial de Compras</h2>
-  {purchaseHistory.length === 0 ? (
-    <p>No purchase history for this user.</p>
-  ) : (
-    <ul className="space-y-4">
-      {purchaseHistory.map((purchase) => (
-        <li
-          key={purchase.purchaseId}
-          className="flex items-center justify-between py-3 border-b border-chiliRed"
-        >
-          <div className="flex items-center space-x-4">
-            <NavLink to={`/detail/${purchase.productId}`} className="flex items-center space-x-4">
-              <img
-                src={purchase.productImage}
-                alt={purchase.productName}
-                className="w-36 h-36 rounded-md"
-              />
-              <div>
-                <p className="text-lg font-semibold">{purchase.productName}</p>
-                <p>Precio: ${purchase.productPrice}</p>
-                <p>Cantidad: {purchase.quantity}</p>
+
+    <div className="text-black p-8 rounded-lg shadow-sm md:w-60vw mx-auto shadow-onyx mt-2 mb-2">
+      <h2 className="text-3xl font-semibold mb-6">Historial de Compras</h2>
+      {purchaseHistory.length === 0 ? (
+        <p>Aun no has realizado ninguna compra.</p>
+      ) : (
+        <ul className="space-y-4">
+          {purchaseHistory.map((purchase) => (
+            <li
+              key={purchase.purchaseId}
+              className="flex items-center justify-between py-3 border-b border-chiliRed"
+            >
+              <div className="flex items-center space-x-4 flex-wrap md:items-center justify-center">
+                <img
+                  src={purchase.productImage}
+                  alt={purchase.productName}
+                  className="w-36 h-36 rounded-md"
+                />
+                <div>
+                  <p className="text-lg font-semibold ">{purchase.productName}</p>
+                  <p>Precio: ${purchase.productPrice}</p>
+                  <p>Cantidad: {purchase.quantity}</p>
+                </div>
+                <div className='p-2 rounded shadow-sm shadow-eerieBlack'>
+                  <NavLink to={`/detail/${purchase.productId}`} className="flex items-center space-x-4">
+                    <h3 className='hover:scale-105'>Volver a comprar</h3>
+                  </NavLink>
+                  <hr className='text-chiliRed'/>
+                  <NavLink>
+                    <h3 className='hover:scale-105 hover:drop-shadow-sm hover:shadow-onyx'>Opinar</h3>
+                  </NavLink>
+                </div>
               </div>
-            </NavLink>
-
-          </div>
-        </li>
-      ))}
-    </ul>
-  )}
-</div>
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
   );
 };
 
