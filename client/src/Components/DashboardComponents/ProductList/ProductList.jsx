b63211e86334509ce77788d224870492d6f0151c
gabrielaDF <defreitasgabi@gmail.com>
2024-01-09 12:37:13 -0500

    responsive de la tabla productList en el dashboard

--- client/src/Components/DashboardComponents/ProductList/ProductList.jsx
+++ client/src/Components/DashboardComponents/ProductList/ProductList.jsx
@@ -12,7 +12,7 @@ import Searchbar from "../../Searchbar/Searchbar";
 
 function ProductList() {
   const { products } = useSelector((state) => state.products);
-
+  const [showMenu, setShowMenu] = useState(true);
   const [showSuccessMessage, setShowSuccessMessage] = useState(false);
   const dispatch = useDispatch();
   const navigate = useNavigate();
@@ -20,7 +20,17 @@ function ProductList() {
   useEffect(() => {
     dispatch(getAllProducts());
   }, [dispatch, showSuccessMessage]);
+  useEffect(() => {
+    const handleResize = () => {
+      setShowMenu(window.innerWidth > 768); // Ajusta el valor 768 según sea necesario
+    };
 
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
   const onClick = async (id) => {
     const deleted = window.confirm("¿Estás seguro de desactivar el producto?");
 
@@ -38,12 +48,12 @@ function ProductList() {
   };
 
   return (
-    <div className="ml-[20%]">
+    <div className="">
       <NavbarAdmin />
-      <div className="container mx-auto mt-0 ml-[10%]">
+      <div className="container mx-auto mt-0 ml-[20%]">
         <Searchbar />
       </div>
-      <div className="container mx-auto mt-6">
+      <div className="w-screen overflow-auto container mx-auto mt-6">
         <h1 className="text-2xl font-bold mb-4 text-center">
           Productos Registrados
         </h1>
@@ -53,8 +63,9 @@ function ProductList() {
             Producto eliminado exitosamente
           </div>
         )}
-        <div className=" w-90vw overflow-x-auto ml-[8%]">
-          <table className="min-w-full bg-whiteSmoke border border-onyx">
+
+        <div className=" w-screen overflow-auto  ml-[18%] hidden md:block">
+          <table className="w-full bg-whiteSmoke border border-onyx ">
             <thead>
               <tr>
                 <th className="py-1 px-1 border-b">Nombre</th>
@@ -65,7 +76,7 @@ function ProductList() {
                 <th className="py-1 px-1 border-b">Imagen</th>
                 <th className="py-1 px-1 border-b">Color</th>
                 <th className="py-1 px-1 border-b">Marca</th>
-                <th className="py-1 px-1 border-b">Categoria</th>
+                <th className="py-1 px-1 border-b">Categoría</th>
                 <th className="py-1 px-1 border-b">Estado</th>
                 <th className="py-1 px-1 border-b">Editar</th>
                 <th className="py-1 px-1 border-b">Eliminar</th>
@@ -111,6 +122,58 @@ function ProductList() {
                 ))}
             </tbody>
           </table>
+        </div>
+        <div className=" grid grid-cols-1  gap-4 md:hidden m-4">
+          <div className="bg-white p-4 rounded-lg shadow">
+            <div className="text-sm space-x-2">
+              <div>
+                {products &&
+                  products.map((product) => (
+                    <div key={product.id}>
+                      <div className="p-2">
+                        <img
+                          src={product.image}
+                          alt={product.name}
+                          className="max-w-[50px] max-h-[50px] mx-auto"
+                        />
+                      </div>
+                      <div className="py-2 px-4 font-bold">
+                        Nombre: {product.name}
+                      </div>
+                      <div className="">
+                        {" "}
+                        Descripción: {product.description}
+                      </div>
+                      <div className=""> Precio: {product.price}</div>
+                      <div className="">
+                        Especificaciones: {product.specifications}
+                      </div>
+                      <div className="">Stock: {product.stock}</div>
+                      <div className="">Color: {product.color}</div>
+                      <div className="">Marca: {product.brand}</div>
+                      <div className="">Categoría: {product.category}</div>
+                      <div className="">
+                        {" "}
+                        Estado:
+                        {product.deleted ? "Inactivo" : "Activo"}
+                      </div>
+                      <div className=" flex border-b space-x-4  p-4 text-xl">
+                        <div className="cursor-pointer ">
+                          <NavLink to={`/dashboard/editProduct/${product.id}`}>
+                            <IoMdCreate />
+                          </NavLink>
+                        </div>
+                        <div className="cursor-pointer">
+                          <div onClick={() => onClick(product.id)}>
+                            <MdDelete />
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  ))}
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
