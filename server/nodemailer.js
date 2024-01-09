2f15cf6a1f96273ab9929e420606b2bf831ab5df
gabrielaDF <defreitasgabi@gmail.com>
2024-01-09 15:34:41 -0500

    antes del pull

--- server/nodemailer.js
+++ server/nodemailer.js
@@ -18,28 +18,31 @@
 // module.exports = transporter
 const nodemailer = require("nodemailer");
 const { google } = require("googleapis");
-require('dotenv').config();
+require("dotenv").config();
 const { CLIENT_ID, CLIENT_SECRET, REDIRECT_URI, REFRESH_TOKEN } = process.env;
 
-const oauth2Client = new google.auth.OAuth2(CLIENT_ID, CLIENT_SECRET, REDIRECT_URI);
+const oauth2Client = new google.auth.OAuth2(
+  CLIENT_ID,
+  CLIENT_SECRET,
+  REDIRECT_URI
+);
 oauth2Client.setCredentials({ refresh_token: REFRESH_TOKEN });
 
-
 const transporter = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-        type: "OAuth2",
-        user: "juan.ruize@gmail.com",
-        clientId: CLIENT_ID,
-        clientSecret: CLIENT_SECRET,
-        refreshToken: REFRESH_TOKEN,
-        accessToken: oauth2Client.getAccessToken(),
-    },
+  service: "gmail",
+  auth: {
+    type: "OAuth2",
+    user: "juan.ruize@gmail.com",
+    clientId: CLIENT_ID,
+    clientSecret: CLIENT_SECRET,
+    refreshToken: REFRESH_TOKEN,
+    accessToken: oauth2Client.getAccessToken(),
+  },
 });
 
-transporter.verify()
-    .then(() => console.log("Email enviado!!"))
-    .catch(error => console.error('Error al verificar el transporte:', error));
+transporter
+  .verify()
+  .then(() => console.log("Email enviado!!"))
+  .catch((error) => console.error("Error al verificar el transporte:", error));
 
 module.exports = transporter;
-
