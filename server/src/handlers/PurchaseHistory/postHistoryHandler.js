f3d38890a84cfddc2c319b127a172b2666fcc63a
Sergio Vallenari <vallenarisergioio@gmail.com>
2024-01-10 18:17:32 -0300

    Flujo de compra pago por transf

---
+++ server/src/handlers/PurchaseHistory/postHistoryHandler.js
@@ -0,0 +1,21 @@
+const postHistoryController = require("../../controllers/PurchaseHistory/postHistoryController")
+
+const postHistoryHandler = async(req, res) => {
+    const {cartItems} = req.body
+    const {userId} = req.params
+    try {
+        const updateProduct = cartItems.map((item) => (
+            {
+                cantidad: item.quantity,
+                idProd: item.idProduct
+    
+            }));
+        await postHistoryController(updateProduct, userId)
+        res.status(200).send('compra agregada')
+    } 
+    catch (error) {
+        res.status(500).send('error al agregar compra')
+    }
+}
+
+module.exports = postHistoryHandler
