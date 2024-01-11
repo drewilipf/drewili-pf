c9dc8d702e96402135eb0147d842dcf84bf29078
Sergio Vallenari <vallenarisergioio@gmail.com>
2024-01-11 16:38:55 -0300

    correccion controlador sortRating

--- server/src/controllers/Product/sortByRatingController.js
+++ server/src/controllers/Product/sortByRatingController.js
@@ -28,18 +28,20 @@ const sortProductsByRatingController = async (order = 'DESC') => {
       ],
     });
 
-    const formattedProducts = productsByRating.map(({ Product: product }) => {
+    const formattedProducts = productsByRating.map((product) => {
       return {
-        id: product.id,
-        name: product.name,
+        idComment: product.id,
+        comment: product.comment,
+        rating: product.rating,
+        name: product.product.name,
         description: product.description,
         price: product.price,
         specifications: product.specifications,
-        color: product.Colors.color,
+        color: product.product.Colors.color,
         stock: product.stock,
         image: product.image,
-        brand: product.Brand.brand,
-        category: product.Category.category,
+        brand: product.product.Brand.brand,
+        category: product.product.Category.category,
         deleted: product.deleted,
         relevance: product.relevance,
         date: product.createdAt,
