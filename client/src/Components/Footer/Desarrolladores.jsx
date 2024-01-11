9289a157fac7252066adc077d78078147425e35d
NicolasSDiez <nicolas.diez1s@gmail.com>
2024-01-11 13:47:18 -0300

    creación de desarrolladores

--- client/src/Components/Footer/Desarrolladores.jsx
+++ client/src/Components/Footer/Desarrolladores.jsx
@@ -27,96 +27,124 @@ const Creators = () => {
                     </div>
                 ))}
                 <div className="absolute top-0 left-0 w-full h-full flex flex-col justify-center items-center">
-                    <div className="shadow-xl grid grid-cols-4 border border-chiliRed rounded-md bg-white gap-4">
-                        <div className="bg-white border border-chiliRed rounded-md p-4 mt-4 w-52 h-48 ">
-                            <div className="bg- rounded-md p-4 mt-4 ">
-                                <h2>Bird's Team</h2>
-                                <p>Quizás, el mejor grupo al cual se puede pertenecer.</p>
+                    <div className="shadow-xl grid grid-cols-4 border border-chiliRed rounded-2xl bg-white gap-4">
+                        <div className=" shadow-2xl border border-chiliRed bg-whiteSmoke rounded-2xl flex items-center flex-col p-2 mb-4 mt-4 ml-4 w-48 h-48">
+                            <div className="bg- rounded-md p-4 mt-4 flex  flex-col">
+                                <h2 className="font-bold text-center">Bird's Team</h2>
+                                <p className="text-center">Quizás, el mejor equipo al cual se pueda pertenecer.</p>
                             </div>
                         </div>
-                        <div className="border border-chiliRed bg-white rounded-md grid grid-col p-4 mt-4 w-48 h-48">
-                            <h2>Sergio Vallenari</h2>
+                        <div className="shadow-2xl border border-chiliRed bg-whiteSmoke rounded-2xl flex items-center flex-col p-2 mb-4 mt-4 w-48 h-48">
+                            <h2 className="font-bold">Sergio Vallenari</h2>
                             <img
                                 src={menProgram}
                                 alt="Descripción de la imagen"
                                 className="w-24 h-24" // Ajusta el tamaño según tus necesidades usando clases de Tailwind
                             />
-                        <div className="flex items-center mr-8 ml-4">
-                            <IoLogoGithub className="w-8 h-8" />
-                            <FaLinkedin className="w-8 h-8"/>
+                            <div className="flex items-center ">.
+                                <a href="https://github.com/SergioFVallenari" target="_blank" rel="noopener noreferrer">
+                                    <IoLogoGithub className="w-8 h-8 mr-4 hover:scale-125 transition-transform duration-300 ease-in-out" />
+                                </a>
+                                <a href="https://www.linkedin.com/in/sergio-vallenari-45737b20a/" target="_blank" rel="noopener noreferrer">
+                                    <FaLinkedin className="w-8 h-8 hover:scale-125 transition-transform duration-300 ease-in-out" />
+                                </a>
+                            </div>
                         </div>
-                        </div>
-                        <div className="border border-chiliRed bg-white rounded-md p-4 mt-4 w-48 h-48">
-                            <h2>Gabi</h2>
+                        <div className="shadow-2xl border border-chiliRed bg-whiteSmoke rounded-2xl flex items-center flex-col p-2 mb-4 mt-4 w-48 h-48">
+                            <h2 className="font-bold">Gabriela de Freitas</h2>
                             <img
                                 src={womanProgram}
                                 alt="Descripción de la imagen"
                                 className="w-24 h-24" // Ajusta el tamaño según tus necesidades usando clases de Tailwind
                             />
-                            <div className="flex items-center mr-8 ml-4">
-                            <IoLogoGithub className="w-8 h-8" />
-                            <FaLinkedin className="w-8 h-8"/>
+                            <div className="flex items-center ">
+                            <a href="https://github.com/gabrielaDF" target="_blank" rel="noopener noreferrer">
+                                    <IoLogoGithub className="w-8 h-8 mr-4 hover:scale-125 transition-transform duration-300 ease-in-out" />
+                                </a>
+                                <a href="https://www.linkedin.com/in/gabriela-de-freitas-90529b121/" target="_blank" rel="noopener noreferrer">
+                                    <FaLinkedin className="w-8 h-8 hover:scale-125 transition-transform duration-300 ease-in-out" />
+                                </a>
+                            </div>
                         </div>
-                        </div>
-                        <div className="border border-chiliRed bg-white rounded-md p-4 mt-4 w-48 h-48">
-                            <h2>Flor</h2>
+                        <div className="shadow-2xl border border-chiliRed bg-whiteSmoke rounded-2xl flex items-center flex-col p-2 mb-4 mt-4 w-48 h-48">
+                            <h2 className="font-bold">Florencia Arévalo</h2>
                             <img
                                 src={womanProgram}
                                 alt="Descripción de la imagen"
                                 className="w-24 h-24" // Ajusta el tamaño según tus necesidades usando clases de Tailwind
                             />
-                            <div className="flex items-center mr-8 ml-4">
-                            <IoLogoGithub className="w-8 h-8" />
-                            <FaLinkedin className="w-8 h-8"/>
+                            <div className="flex items-center ">
+                            <a href="https://github.com/MFArevaloCordoba" target="_blank" rel="noopener noreferrer">
+                                    <IoLogoGithub className="w-8 h-8 mr-4 hover:scale-125 transition-transform duration-300 ease-in-out" />
+                                </a>
+                                <a href="https://www.linkedin.com/in/florencia-ar%C3%A9valo-148271275/" target="_blank" rel="noopener noreferrer">
+                                    <FaLinkedin className="w-8 h-8 hover:scale-125 transition-transform duration-300 ease-in-out" />
+                                </a>
+                            </div>
                         </div>
+                        <div className="shadow-2xl border border-chiliRed bg-whiteSmoke rounded-2xl flex items-center flex-col p-2 mb-4 ml-4 w-48 h-48">
+                            <h2 className="font-bold">Juan Ruiz</h2>
+                            <img
+                                src={menProgram}
+                                alt="Descripción de la imagen"
+                                className="w-24 h-24" 
+                            />
+                            <div className="flex items-center ">
+                            <a href="https://github.com/juanruize" target="_blank" rel="noopener noreferrer">
+                                    <IoLogoGithub className="w-8 h-8 mr-4 hover:scale-125 transition-transform duration-300 ease-in-out" />
+                                </a>
+                                <a href="https://www.linkedin.com/in/juan-guillermo-ruiz-32b76513a/" target="_blank" rel="noopener noreferrer">
+                                    <FaLinkedin className="w-8 h-8 hover:scale-125 transition-transform duration-300 ease-in-out" />
+                                </a>
+                            </div>
                         </div>
-                        <div className="border border-chiliRed bg-white rounded-md p-4 mt-4 w-48 h-48">
-                            <h2>Juan</h2>
+                        <div className="shadow-2xl border border-chiliRed bg-whiteSmoke rounded-2xl flex items-center flex-col p-2 mb-4  w-48 h-48">
+                            <h2 className="font-bold">Nicolás Diez</h2>
                             <img
                                 src={menProgram}
                                 alt="Descripción de la imagen"
                                 className="w-24 h-24" // Ajusta el tamaño según tus necesidades usando clases de Tailwind
                             />
-                           <div className="flex items-center mr-8 ml-4">
-                            <IoLogoGithub className="w-8 h-8" />
-                            <FaLinkedin className="w-8 h-8"/>
+                            <div className="flex items-center ">
+                            <a href="https://github.com/NicolasSDiez" target="_blank" rel="noopener noreferrer">
+                                    <IoLogoGithub className="w-8 h-8 mr-4 hover:scale-125 transition-transform duration-300 ease-in-out" />
+                                </a>
+                                <a href="https://www.linkedin.com/in/nicolas-sdiez/" target="_blank" rel="noopener noreferrer">
+                                    <FaLinkedin className="w-8 h-8 hover:scale-125 transition-transform duration-300 ease-in-out" />
+                                </a>
+                            </div>
                         </div>
-                        </div>
-                        <div className="border border-chiliRed bg-white rounded-md p-4 mt-4 w-48 h-48">
-                            <h2>Nico</h2>
+                        <div className="shadow-2xl border border-chiliRed bg-whiteSmoke rounded-2xl flex items-center flex-col p-2 mb-4  w-48 h-48">
+                            <h2 className="font-bold">Mauricio Collado</h2>
                             <img
                                 src={menProgram}
                                 alt="Descripción de la imagen"
                                 className="w-24 h-24" // Ajusta el tamaño según tus necesidades usando clases de Tailwind
                             />
-                            <div className="flex items-center mr-8 ml-4">
-                            <IoLogoGithub className="w-8 h-8" />
-                            <FaLinkedin className="w-8 h-8"/>
+                            <div className="flex items-center ">
+                            <a href="https://github.com/Elranas5000" target="_blank" rel="noopener noreferrer">
+                                    <IoLogoGithub className="w-8 h-8 mr-4 hover:scale-125 transition-transform duration-300 ease-in-out" />
+                                </a>
+                                <a href="https://www.linkedin.com/in/mauricio-collado-455835241/" target="_blank" rel="noopener noreferrer">
+                                    <FaLinkedin className="w-8 h-8 hover:scale-125 transition-transform duration-300 ease-in-out" />
+                                </a>
+                            </div>
                         </div>
-                        </div>
-                        <div className="border border-chiliRed bg-white rounded-2xl p-4 mt-4 w-48 h-48">
-                            <h2>Mauri</h2>
-                            <img
-                                src={menProgram}
-                                alt="Descripción de la imagen"
-                                className="w-24 h-24" // Ajusta el tamaño según tus necesidades usando clases de Tailwind
-                            />
-                           <div className="flex items-center mr-8 ml-4">
-                            <IoLogoGithub className="w-8 h-8" />
-                            <FaLinkedin className="w-8 h-8"/>
-                        </div>
-                        </div>
-                        <div className="border border-chiliRed bg-white rounded-2xl p-4 mt-4 w-48 h-48">
-                            <h2>Juli</h2>
+                        <div className="shadow-2xl border border-chiliRed bg-whiteSmoke rounded-2xl flex items-center flex-col p-2 mb-4  w-48 h-48">
+                            <h2 className="font-bold">Juliana FLorez</h2>
                             <img
                                 src={womanProgram}
                                 alt="Descripción de la imagen"
-                                className="w-24 h-24" // Ajusta el tamaño según tus necesidades usando clases de Tailwind
+                                className="w-24 h-24 flex items-center" // Ajusta el tamaño según tus necesidades usando clases de Tailwind
                             />
-                            <div className="flex items-center mr-8 ml-4">
-                            <IoLogoGithub className="w-8 h-8" />
-                            <FaLinkedin className="w-8 h-8"/>
-                        </div>
+                            <div className="flex items-center">
+                            <a href="https://github.com/JFlorezQ" target="_blank" rel="noopener noreferrer">
+                                    <IoLogoGithub className="w-8 h-8 mr-4 hover:scale-125 transition-transform duration-300 ease-in-out" />
+                                </a>
+                                <a href="https://www.linkedin.com/in/juliana-florez-quiros-483801229/" target="_blank" rel="noopener noreferrer">
+                                    <FaLinkedin className="w-8 h-8 hover:scale-125 transition-transform duration-300 ease-in-out" />
+                                </a>
+                            </div>
                         </div>
 
                         {/* Agrega más divs con información según sea necesario */}
--- client/src/Components/Footer/Footer.jsx
+++ client/src/Components/Footer/Footer.jsx
@@ -3,8 +3,8 @@ import LegalesLink from "./LinkLegales";
 import instagramIcon from "../../icons/instagram.png";
 import linkedinIcon from "../../icons/linkedin.png";
 import whatsappIcon from "../../icons/whatsapp.png";
-import Creators from "./Desarrolladores";
 import { Link } from "react-router-dom";
+import { LuBird } from "react-icons/lu";
 
 const Footer = () => {
   return (
@@ -65,7 +65,7 @@ const Footer = () => {
         </div>
       </footer>
      <div className="text-center bg-eerieBlack bg-opacity-40 text-white">
-      <Link to="/creators" >Desarrolladores</Link>
+      <Link to="/creators" className="flex items-center justify-center" >Desarrolladores<LuBird className="ml-4" /></Link>
      </div>
       <h4 className="text-center bg-onyx text-white">© 2024 DREWILI Todos los derechos reservados.</h4>
 
