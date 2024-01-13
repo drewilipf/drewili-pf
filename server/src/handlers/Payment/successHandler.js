ee945113e2fe302565af24de3f0621ef46fdc132
Sergio Vallenari <vallenarisergioio@gmail.com>
2024-01-13 19:51:32 -0300

    Modificacion controladores varios

--- server/src/handlers/Payment/successHandler.js
+++ server/src/handlers/Payment/successHandler.js
@@ -4,12 +4,10 @@ const successHandler = async (req, res) => {
     try {
         const userId = req.query.userId;
         const updateProduct = JSON.parse(req.query.updateProduct);
-        console.log(userId, 'este es el usuario en la compra');
-        console.log(updateProduct, 'update product del success');
         
-        await successController(userId, updateProduct);
-        
-        res.redirect('https://drewilifront.vercel.app/payment/success');
+        const data = await successController(userId, updateProduct);
+        const id = data.length > 0 ? data[0].id : null
+        res.redirect(`https://drewilifront.vercel.app/payment/success?id=${id}`);
     } catch (error) {
         res.status(500).send('Error en el servidor');
     }
