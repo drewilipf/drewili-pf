8c54ff288cb7e8e4275b918d460e04e1c2348dce
gabrielaDF <defreitasgabi@gmail.com>
2024-01-14 18:54:35 -0500

    instalar mixpanel

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -6,18 +6,21 @@ import { BrowserRouter } from "react-router-dom";
 import { store } from "./reduxToolkit/store.js";
 import { Provider } from "react-redux";
 import { Auth0Provider } from "@auth0/auth0-react";
+import mixpanel from "mixpanel-browser";
 
+mixpanel.init("11d697acdd2e4090745feababa6ed22d");
 ReactDOM.createRoot(document.getElementById("root")).render(
-    <Provider store={store}>
-      <BrowserRouter>
-        <Auth0Provider
-          domain="drewili.us.auth0.com"
-          clientId="1D6DJezLcvz5RAM9cqQs4q0ukKUSe6nI"
-          authorizationParams={{
-            redirect_uri: window.location.origin
-          }}>
-          <App />
-        </Auth0Provider>
-      </BrowserRouter>
-    </Provider>
+  <Provider store={store}>
+    <BrowserRouter>
+      <Auth0Provider
+        domain="drewili.us.auth0.com"
+        clientId="1D6DJezLcvz5RAM9cqQs4q0ukKUSe6nI"
+        authorizationParams={{
+          redirect_uri: window.location.origin,
+        }}
+      >
+        <App />
+      </Auth0Provider>
+    </BrowserRouter>
+  </Provider>
 );
