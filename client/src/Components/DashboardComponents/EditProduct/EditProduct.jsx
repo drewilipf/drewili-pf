ef0d3791b2aa69c2d0ca11e794893d2f97e141ad
Sergio Vallenari <vallenarisergioio@gmail.com>
2024-01-13 21:41:17 -0300

    Correccion bug del editado del producto

--- client/src/Components/DashboardComponents/EditProduct/EditProduct.jsx
+++ client/src/Components/DashboardComponents/EditProduct/EditProduct.jsx
@@ -18,7 +18,10 @@ const EditProduct = () => {
   const styles2 = "text-eerieBlack text-lg";
   const styles =
     "w-full px-8 py-1.5 text-lg text-eerieBlack leading-tight bg-whiteSmoke border rounded focus:outline-none focus:shadow-outline";
-  console.log(id);
+  const { categories } = useSelector((state) => state.categories);
+  const { brands } = useSelector((state) => state.brands);
+  const { color } = useSelector((state) => state.color);
+
   const [input, setInput] = useState({
     name: "",
     description: "",
@@ -31,15 +34,11 @@ const EditProduct = () => {
     brand_id: 0,
     deleted: false,
   });
-  const { categories } = useSelector((state) => state.categories);
-  const { brands } = useSelector((state) => state.brands);
-  const { color } = useSelector((state) => state.color);
+
   const [imageFile, setImageFile] = useState([]);
   const productsId = useSelector((state) => state.products.productsId);
-  console.log(imageFile, 'array de imagefile');
-  console.log(productsId);
   const product = productsId.length > 0 ? productsId[0] : null;
-  console.log(product);
+
   useEffect(() => {
     const fetchData = async () => {
       await dispatch(getProductsById(id));
@@ -50,18 +49,7 @@ const EditProduct = () => {
 
     fetchData();
   }, [id, dispatch]);
-  if (!product || !color) {
-    return <p>Cargando...</p>;
-  }
-  const colorId = product?.color
-    ? color.find((c) => c.color === product.color)?.id || 0
-    : 0;
-  const brandId = product?.brand
-    ? brands.find((b) => b.brand === product.brand)?.id || 0
-    : 0;
-  const categoryId = product?.category
-    ? categories.find((cat) => cat.category === product.category)?.id || 0
-    : 0;
+
   useEffect(() => {
     if (product) {
       setInput({
@@ -71,55 +59,58 @@ const EditProduct = () => {
         specifications: product.specifications,
         stock: parseInt(product.stock),
         imageArray: product.imageArray[0],
-        color_id: colorId,
-        category_id: categoryId,
-        brand_id: brandId,
-        deleted: product.deleted || "false",
+        color_id: color.find((c) => c.color === product.color)?.id || 0,
+        category_id: categories.find((cat) => cat.category === product.category)?.id || 0,
+        brand_id: brands.find((b) => b.brand === product.brand)?.id || 0,
+        deleted: product.deleted || false,
       });
     }
-  }, [product]);
-  function handleColorSelect(event) {
+  }, [product, color, categories, brands]);
+
+  const handleColorSelect = (event) => {
     setInput({
       ...input,
       color_id: event.target.value,
     });
-  }
+  };
 
-  function handleBrandSelect(event) {
+  const handleBrandSelect = (event) => {
     setInput({
       ...input,
       brand_id: event.target.value,
     });
-  }
+  };
 
-  function handleCategorySelect(event) {
+  const handleCategorySelect = (event) => {
     setInput({
       ...input,
       category_id: event.target.value,
     });
-  }
-  function handleChange(event) {
+  };
+
+  const handleChange = (event) => {
     setInput({
       ...input,
       [event.target.name]: event.target.value,
     });
-  }
-  function handleImageChange(event) {
+  };
+
+  const handleImageChange = (event) => {
     const file = event.target.files;
     setImageFile((prev) => [...prev, ...file]);
-  }
-  async function handleSumit(event) {
+  };
+
+  const handleSumit = async (event) => {
     event.preventDefault();
     try {
       let arrayUrls = [];
-      console.log(arrayUrls, 'array de imagenes');
-      if (imageFile) {
+      if (imageFile.length > 0) {
         await Promise.all(
           imageFile.map(async (img) => {
             const formData = new FormData();
             formData.append("file", img);
             formData.append("upload_preset", "wagnbv9p");
-  
+
             const response = await fetch(
               "https://api.cloudinary.com/v1_1/dpj4n40t6/image/upload",
               {
@@ -127,7 +118,7 @@ const EditProduct = () => {
                 body: formData,
               }
             );
-  
+
             const data = await response.json();
             const imageUrl = data.secure_url;
             arrayUrls.push(imageUrl);
@@ -142,15 +133,15 @@ const EditProduct = () => {
         specifications: input.specifications,
         stock: parseInt(input.stock),
         imageArray: arrayUrls.length > 0 ? arrayUrls : product.imageArray,
-        color_id: input.color ? parseInt(input.color) : colorId,
-        category_id: input.category ? parseInt(input.category) : categoryId,
-        brand_id: input.brand ? parseInt(input.brand) : brandId,
+        color_id: parseInt(input.color_id),
+        category_id: parseInt(input.category_id),
+        brand_id: parseInt(input.brand_id),
         deleted: input.deleted,
       };
-      console.log("productData", productData);
+
       await dispatch(putProduct(id, productData));
 
-      alert("producto actualizado con éxito");
+      alert("Producto actualizado con éxito");
 
       setInput({
         name: product.name,
@@ -159,17 +150,22 @@ const EditProduct = () => {
         specifications: product.specifications,
         stock: parseInt(product.stock),
         imageArray: product.imageArray,
-        color_id: colorId,
-        category_id: categoryId,
-        brand_id: brandId,
-        deleted: product.deleted || "false",
+        color_id: parseInt(input.color_id),
+        category_id: parseInt(input.category_id),
+        brand_id: parseInt(input.brand_id),
+        deleted: input.deleted,
       });
 
       navigate("/dashboard/productList");
     } catch (error) {
       alert("Error actualizando el producto");
     }
+  };
+
+  if (!product || !color || !brands || !categories) {
+    return <p>Cargando...</p>;
   }
+
 
   return (
     <div className="max-w-md mx-auto mt-30 mr-50">
@@ -266,7 +262,7 @@ const EditProduct = () => {
               onChange={handleColorSelect}
               className="border rounded p-3 w-full bg-whiteSmoke focus:outline-none"
             >
-              <option value={colorId} key="empty">
+              <option value={input.color_id} key="empty">
                 Selecciona un color
               </option>
               {color?.map((element) => {
@@ -286,7 +282,7 @@ const EditProduct = () => {
               onChange={handleBrandSelect}
               className="border rounded p-3 w-full bg-whiteSmoke focus:outline-none"
             >
-              <option value={brandId} key="empty">
+              <option value={input.brand_id} key="empty">
                 Selecciona una marca
               </option>
               {brands?.map((element) => {
@@ -306,7 +302,7 @@ const EditProduct = () => {
               onChange={handleCategorySelect}
               className="border rounded p-3 w-full bg-whiteSmoke focus:outline-none"
             >
-              <option value={categoryId} key="empty">
+              <option value={input.category_id} key="empty">
                 Selecciona una categoria
               </option>
               {categories?.map((element) => {
