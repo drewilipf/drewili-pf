9912a35c8e7c4d6bf88f725c0a6f14ecaac80866
gabrielaDF <defreitasgabi@gmail.com>
2024-01-14 10:29:31 -0500

    datos faltantes en paymentsuccess

--- client/src/views/Payment/PaymentSuccess.jsx
+++ client/src/views/Payment/PaymentSuccess.jsx
@@ -4,49 +4,50 @@ import { NavLink } from "react-router-dom";
 import generatePDF from "../Shoppingcart/createPdfEnvio";
 
 const PaymentSuccess = () => {
-  useEffect(() => {
-    const queryString = window.location.search;
+  // useEffect(() => {
+  const queryString = window.location.search;
 
-    // Crea un nuevo objeto URLSearchParams y pásale la cadena de consulta
-    const urlParams = new URLSearchParams(queryString);
+  // Crea un nuevo objeto URLSearchParams y pásale la cadena de consulta
+  const urlParams = new URLSearchParams(queryString);
 
-    // Obtén el valor del parámetro "id"
-    const purchaseId = urlParams.get("id");
+  // Obtén el valor del parámetro "id"
+  const purchaseId = urlParams.get("id");
 
-    // Ahora 'purchaseId' contiene el valor del parámetro "id"
-    console.log(purchaseId);
-    const { shippingInfo } = useSelector((state) => state.shipping);
+  // Ahora 'purchaseId' contiene el valor del parámetro "id"
+  console.log(purchaseId);
+  const { shippingInfo } = useSelector((state) => state.shipping);
 
-    const { dropshippingInfo } = useSelector((state) => state.shipping);
+  const { dropshippingInfo } = useSelector((state) => state.shipping);
 
-    const { opcionQuienRecibe } = useSelector((state) => state.shipping);
-    const { opciontipoComprobante } = useSelector((state) => state.shipping);
-    const { razonSocialFactura } = useSelector((state) => state.shipping);
-    const { rucFactura } = useSelector((state) => state.shipping);
+  const { opcionQuienRecibe } = useSelector((state) => state.shipping);
+  const { opciontipoComprobante } = useSelector((state) => state.shipping);
+  const { razonSocialFactura } = useSelector((state) => state.shipping);
+  const { rucFactura } = useSelector((state) => state.shipping);
+  const { modalidadPago } = useSelector((state = state.shipping));
 
-    const opcionR = opcionQuienRecibe;
-    const opcionC = opciontipoComprobante;
-    const combinedData = {
-      Nombre: shippingInfo.name,
-      Apellido: shippingInfo.lastname,
-      Dirección: shippingInfo.address,
-      "Correo electrónico": shippingInfo.email,
-      Celular: shippingInfo.phone,
-      "Nº de Documento": shippingInfo.dni,
-      Dropshipping: "Datos del Cliente",
-      "Nombre completo ": dropshippingInfo.name,
-      "Dirección de envío": dropshippingInfo.address,
-      Teléfono: dropshippingInfo.phone,
-      "Número de Documento": dropshippingInfo.dni,
-      "¿Quién recibirá el pedido?": opcionR,
-      "¿Qué tipo de comprobante desea?": opcionC,
-      "Razón Social": razonSocialFactura,
-      RUC: rucFactura,
-      "modalidad de pago": modalidadPago,
-    };
-    console.log(combinedData);
-    generatePDF(combinedData, purchaseId);
-  }, []);
+  const opcionR = opcionQuienRecibe;
+  const opcionC = opciontipoComprobante;
+  const combinedData = {
+    Nombre: shippingInfo.name,
+    Apellido: shippingInfo.lastname,
+    Dirección: shippingInfo.address,
+    "Correo electrónico": shippingInfo.email,
+    Celular: shippingInfo.phone,
+    "Nº de Documento": shippingInfo.dni,
+    Dropshipping: "Datos del Cliente",
+    "Nombre completo ": dropshippingInfo.name,
+    "Dirección de envío": dropshippingInfo.address,
+    Teléfono: dropshippingInfo.phone,
+    "Número de Documento": dropshippingInfo.dni,
+    "¿Quién recibirá el pedido?": opcionR,
+    "¿Qué tipo de comprobante desea?": opcionC,
+    "Razón Social": razonSocialFactura,
+    RUC: rucFactura,
+    "modalidad de pago": modalidadPago,
+  };
+  console.log(combinedData);
+  generatePDF(combinedData, purchaseId);
+  // }, []);
   return (
     <div className="h-90vh  bg-whiteSmoke flex justify-center items-center flex-col">
       <article className=" h-[50%] w-[50%] rounded-2xl shadow-2xl flex justify-center flex-col items-center">
