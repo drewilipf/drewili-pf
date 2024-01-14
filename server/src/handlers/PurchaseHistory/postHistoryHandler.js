3a95e7a17dc33b45a831396bb10db930eff30500
gabrielaDF <defreitasgabi@gmail.com>
2024-01-14 00:20:00 -0500

    completa la actualicacion del documento pdf para pago sin la pasarela

--- server/src/handlers/PurchaseHistory/postHistoryHandler.js
+++ server/src/handlers/PurchaseHistory/postHistoryHandler.js
@@ -8,20 +8,6 @@ const postHistoryHandler = async (req, res) => {
       cantidad: item.quantity,
       idProd: item.idProduct,
     }));
-    await postHistoryController(updateProduct, userId);
-    res.status(200).send("compra agregada");
-  } catch (error) {
-    res.status(500).send("error al agregar compra");
-  }
-};
-const postHistoryHandler = async (req, res) => {
-  const { cartItems } = req.body;
-  const { userId } = req.params;
-  try {
-    const updateProduct = cartItems.map((item) => ({
-      cantidad: item.quantity,
-      idProd: item.idProduct,
-    }));
     const newPurchase = await postHistoryController(updateProduct, userId);
     res.status(200).send(newPurchase);
   } catch (error) {
