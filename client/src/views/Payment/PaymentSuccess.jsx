780545d9ff37cbfb8ac62ec9849c08e255e30fe6
Juliana <jflorez@proton.me>
2024-01-16 12:25:21 -0500

    Creo que lo de correos con la tarjeta ya funciona

--- client/src/views/Payment/PaymentSuccess.jsx
+++ client/src/views/Payment/PaymentSuccess.jsx
@@ -3,12 +3,40 @@ import { useDispatch, useSelector } from "react-redux";
 import { NavLink, useLocation } from "react-router-dom";
 import generatePDF from "../Shoppingcart/createPdfEnvio";
 import Cookies from "js-cookie";
+import { postNotificationUserConfirmBuy } from "../../reduxToolkit/Notification/notificationThunks";
+import { postNotificationAdminConfirmBuy } from "../../reduxToolkit/Notification/notificationThunks";
+import mixpanel from "mixpanel-browser";
 
 const PaymentSuccess = () => {
   const location = useLocation();
-  const emailData = location.state;
+
+  const {emailData} = useSelector((state) => state.notification);
+
+  console.log("emaildata estado global", emailData)
+
+  const [usermaildata, setUsermaildata] = useState({
+    name: "",
+    email: "",
+    products: [],
+    total: 0,
+    adress: "",
+    status: "default",
+  });
+
+  const [adminmaildata, setAdminmaildata] = useState({
+    name: "",
+    email: "",
+    phone: "",
+    products: [],
+    total: 0,
+    adress: "",
+    dropshiping: "",
+    payment: "",
+    status: "default",
+  });
+  
   useEffect(() => {
-    console.log(emailData);
+    console.log("emailData", emailData);
     if (emailData) {
       setUsermaildata({
         name: emailData.name || "",
@@ -17,10 +45,27 @@ const PaymentSuccess = () => {
           name: item.name,
           quantity: item.quantity,
           price: item.price,
-        })),
+        })), 
         total: emailData.totalprice || 0,
       });
+      setAdminmaildata({
+        name: emailData.name || "",
+        email: emailData.email || "",
+        phone: emailData.phone || "",
+        products: emailData.product?.map((item) => ({
+          name: item.name,
+          quantity: item.quantity,
+          price: item.price,
+        })),
+        adress: emailData.adress || "",
+
+        total: emailData.totalprice || 0,
+        dropshipping: emailData.dropshipping || "",
+        status: "default",
+        //status: emailData.status
+      });
     }
+    
     mixpanel.track("CompraCompletada", {
       productos: [
         {
@@ -33,7 +78,8 @@ const PaymentSuccess = () => {
       ],
       total: emailData.totalprice, // Agrega el total de la compra
     });
-  }, []);
+  }, [emailData]);
+
   useEffect(() => {
     const queryString = window.location.search;
 
@@ -45,15 +91,27 @@ const PaymentSuccess = () => {
 
     // Ahora 'purchaseId' contiene el valor del parámetro "id"
     console.log(purchaseId);
+    console.log("emaildata estado global", emailData)
 
     const combinedDataFromCookie = Cookies.get("combinedData");
     const combinedData = combinedDataFromCookie
       ? JSON.parse(combinedDataFromCookie)
       : null;
-    console.log(combinedData);
+    console.log("combined data",combinedData);
     generatePDF(combinedData, purchaseId);
   }, []);
 
+
+  useEffect(() => {
+    if (usermaildata && usermaildata.name) {
+      console.log("Datos email usuario:", usermaildata);
+      dispatch(postNotificationUserConfirmBuy(usermaildata));
+    }
+    if (adminmaildata && adminmaildata.name) {
+      console.log("Datos email administrador:", adminmaildata);
+      dispatch(postNotificationAdminConfirmBuy(adminmaildata));
+    }
+  }, [usermaildata, adminmaildata, dispatch]);
   return (
     <div className="h-90vh  bg-whiteSmoke flex justify-center items-center flex-col">
       <article className=" h-[50%] w-[50%] rounded-2xl shadow-2xl flex justify-center flex-col items-center">
