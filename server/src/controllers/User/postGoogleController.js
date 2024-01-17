c1f97ae3e80ec7d3d5b39a577c3ac23fb52ca334
gabrielaDF <defreitasgabi@gmail.com>
2024-01-17 14:01:44 -0500

    cambios depues del pull

--- server/src/controllers/User/postGoogleController.js
+++ server/src/controllers/User/postGoogleController.js
@@ -1,21 +1,21 @@
-const { User } = require("../../db")
+const { User } = require("../../db");
 
-const postGoogleController = async(userName, authId, email) =>{
-    const userGoogle = await User.findOne({
-        where: {auth0UserId: authId}
-    })
+const postGoogleController = async (userName, authId, email) => {
+  const userGoogle = await User.findOne({
+    where: { auth0UserId: authId },
+  });
 
-    if (!userGoogle) {
-        const userGoogle = await User.create({
-            auth0UserId: authId,
-            auth0Email: email,
-            auth0DisplayName: userName,
-            username: userName,
-            email: email
-        })
-        return userGoogle
-    }
-    return userGoogle
-}
+  if (!userGoogle) {
+    const userGoogle = await User.create({
+      auth0UserId: authId,
+      auth0Email: email,
+      auth0DisplayName: userName,
+      username: userName,
+      email: email,
+    });
+    return userGoogle;
+  }
+  return userGoogle;
+};
 
-module.exports = postGoogleController
+module.exports = postGoogleController;
--- server/src/handlers/User/postGoogleHandler.js
+++ server/src/handlers/User/postGoogleHandler.js
@@ -1,14 +1,13 @@
-const postGoogleController = require("../../controllers/User/postGoogleController")
+const postGoogleController = require("../../controllers/User/postGoogleController");
 
-const postGoogleHandler = async(req, res) =>{
-    try {
-        const {userName, authId, email } = req.body
-        const userGoogle = await postGoogleController(userName, authId, email)
-        res.status(200).json(userGoogle)
-    } 
-    catch (error) {
-        res.status(500).json({error: error.message})
-    }
-}
+const postGoogleHandler = async (req, res) => {
+  try {
+    const { userName, authId, email } = req.body;
+    const userGoogle = await postGoogleController(userName, authId, email);
+    res.status(200).json(userGoogle);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
 
-module.exports = postGoogleHandler
+module.exports = postGoogleHandler;
